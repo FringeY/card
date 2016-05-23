3831c37b86253c1048acdbe2749768302ed9696e
yaorui <465964690@qq.com>
2016-05-24 03:27:55 +0800

    debug

--- src/js/main.js
+++ src/js/main.js
@@ -116,10 +116,10 @@ const route = {
     },
     parse() {
         let hash = decodeURIComponent(location.hash);
-        let type = (hash.match(/type=(?:(\w+)&)?/) ? location.hash.match(/type=(?:(\w+)&)?/)[1] : 'form') || 'form';
-        info.name = (hash.match(/name=(?:(\w+)&)?/) ? location.hash.match(/name=(?:([\w\u4e00-\u9fa5]+)&)?/)[1] : '') || '';
-        info.company = (hash.match(/company=(?:(\w+)&)?/) ? location.hash.match(/company=(?:(\w+)&)?/)[1] : 0) || 0;
-        info.job = (hash.match(/job=(?:(\w+)&)?/) ? location.hash.match(/job=(?:(\w+)&)?/)[1] : 0) || 0;
+        let type = (hash.match(/type=(?:(\w+)&)?/) ? hash.match(/type=(?:(\w+)&)?/)[1] : 'form') || 'form';
+        info.name = (hash.match(/name=(?:(\w+)&)?/) ? hash.match(/name=(?:([\w\u4e00-\u9fa5]+)&)?/)[1] : '') || '';
+        info.company = (hash.match(/company=(?:(\w+)&)?/) ? hash.match(/company=(?:(\w+)&)?/)[1] : 0) || 0;
+        info.job = (hash.match(/job=(?:(\w+)&)?/) ? hash.match(/job=(?:(\w+)&)?/)[1] : 0) || 0;
         return type;
     },
     render(type) {  
