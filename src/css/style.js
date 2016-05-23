442e48657d5cca73546fc2c0f7eceb7561ccd562
yaorui <465964690@qq.com>
2016-05-23 22:51:42 +0800

    build

--- src/css/style.js
+++ src/css/style.js
@@ -2,4 +2,5 @@ import '../../node_modules/normalize.css';
 import './style.less';
 import '../img/title.png';
 import '../img/follow.png';
+import '../img/tencent.png';
 import '../../node_modules/lib-flexible/flexible.js';
--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,25 @@
 
 const card = {
     init() {
+        let canvasRect = document.getElementById('card').getBoundingClientRect();
         let ctx = document.getElementById('card').getContext('2d');
         let img = new Image();
+        let newImg = {
+            width: canvasRect.right - canvasRect.left,
+            width: canvasRect.bottom - canvasRect.top
+        };
         img.onload = function () {
             ctx.drawImage(img, 0, 0, 300, 220);
             ctx.font = "48px serif";
             ctx.fillText("Hello world", 10, 50);
-        }
-        img.src = 'src/img/tencent.png';
+        };
+        img.src = 'public/build/img/tencent.png';
     },
     draw() {
 
     }
 };
 
-card.init();
+window.onload = function () {
+    card.init();    
+};
